ca097bf2afc44a794a41abf25dddbec1f51eced4
royce <kim.royce2@gmail.com>
2017-12-03 20:01:42 -0500

    Add test endpoints

---
+++ app/controllers/v1/rooms_controller.rb
@@ -0,0 +1,32 @@
+class V1::RoomsController < ApplicationController
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      render 'v1/rooms/show'
+    else
+      render json: @room.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    @room.update(room_params)
+    render 'v1/rooms/show'
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    render 'v1/rooms/show'
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:pin, :hasMordred, :hasOberon, :hasPercival, :current_mission, :players)
+  end
+end
