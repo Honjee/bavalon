a00b52d6359ee9086749bf3cf55e2e16a47d7528
royce <kim.royce2@gmail.com>
2017-12-04 02:49:02 -0500

    Integrate createreactapp

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/avalon',
+  output: {
+    filename: './app/assets/javascripts/components/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.svg', '*']
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+
+    ]
+  },
+
+  devtool: 'source-maps'
+};
