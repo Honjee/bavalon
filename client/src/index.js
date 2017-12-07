caaecac034f623c7926d8d6eef0e90fb625d1151
royce <kim.royce2@gmail.com>
2017-12-07 02:37:58 -0500

    Add root component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/avalon';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const setupStore = () => {
+  let store
+  store = window.currentUser ?
+            configureStore({ session: window.currentUser }) :
+            configureStore()
+
+  return store
+}
+
+
+ReactDOM.render(<App store={setupStore()} />, document.getElementById('root'));
 registerServiceWorker();
