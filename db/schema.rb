2b942073fc6afe5f7255255537f4c0e1ff897fe0
royce <kim.royce2@gmail.com>
2017-12-09 02:36:44 -0500

    add owner to room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203184319) do
+ActiveRecord::Schema.define(version: 20171209062924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171203184319) do
     t.integer "current_mission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "owner_id"
     t.index ["pin"], name: "index_rooms_on_pin", unique: true
   end
 
