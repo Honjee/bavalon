ccd1f0bef5cc3b744250b133021921bc63ed57c0
royce <kim.royce2@gmail.com>
2017-12-09 21:21:23 -0500

    add name to rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209062924) do
+ActiveRecord::Schema.define(version: 20171209174817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171209062924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "owner_id"
+    t.string "name", null: false
     t.index ["pin"], name: "index_rooms_on_pin", unique: true
   end
 
