d221bc2804b8298478e796218eaf38c4cb7bb775
royce <kim.royce2@gmail.com>
2018-01-15 21:24:09 -0500

    add players reducer

--- client/src/root_reducer.js
+++ client/src/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux-immutable'
 import session from './components/auth_form/reducer.js'
 import home from './components/home_container/reducer.js'
 import room from './components/home_container/create_room_container/reducer.js'
+import players from './components/room_container/reducer'
 
 const RootReducer = combineReducers({
   session,
   home,
-  room
+  room,
+  players
 })
 
 export default RootReducer
