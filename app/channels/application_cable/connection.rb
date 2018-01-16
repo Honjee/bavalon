10776024960509f01eb45068bfa39fda1ef49e66
royce <kim.royce2@gmail.com>
2018-01-15 21:21:07 -0500

    add websocket skeleton

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
+require 'byebug'
+
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      return true
+      # Implement auth check
+      if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :users, only: [:index, :show, :create, :update]
     resource :session, only: [:create, :destroy]
-    resources :rooms, only: [:show, :create, :update]
+    resources :rooms, only: [:create, :update]
+
+    resources :rooms, only: [:show] do
+      resources :players, only: [:show, :create, :update]
+    end
   end
+
+  mount ActionCable.server => '/cable'
 
   get '*path', to: "application#fallback_index_html", constraints: -> (request) do
     !request.xhr? && request.format.html?
