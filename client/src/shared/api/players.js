87facae8119c8671bdd3577ebf35ea989cf78fe2
royce <kim.royce2@gmail.com>
2018-01-15 21:11:40 -0500

    add frontend api endpoints

---
+++ client/src/shared/api/players.js
@@ -0,0 +1,27 @@
+import { ajax } from 'jquery'
+
+const createRoomPlayers = (roomId) => {
+  return ajax({
+    url: `/v1/rooms/${roomId}/players`,
+    data: { roomId },
+    method: 'POST'
+  })
+}
+
+const getRoomPlayers = ({ roomId }) => {
+  debugger
+  return ajax({
+    url: `/v1/rooms/${roomId}/players`,
+    method: 'GET'
+  })
+}
+
+const updateRoomPlayers = (players) => {
+  return ajax({
+    url: `/v1/rooms/${players.roomId}/players`,
+    data: { players },
+    method: 'PATCH'
+  })
+}
+
+export { createRoomPlayers, getRoomPlayers, updateRoomPlayers }
