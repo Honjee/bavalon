54abb3e44fbe3561397cedcdbb11b78d41ab4fdf
royce <kim.royce2@gmail.com>
2018-03-17 18:38:59 -0400

    update subscription to rooms

--- client/src/shared/util/connection.js
+++ client/src/shared/util/connection.js
@@ -1,22 +1,12 @@
 import ActionCable from 'actioncable'
 
-export const createConnection = channel => {
+export const createConnection = (channel, roomId) => {
   const App = {}
   App.cable = ActionCable.createConsumer()
-  App.room = App.cable.subscriptions.create(
-    channel, {
-      connected: (data) => {
-        debugger
-      },
-      disconnected: (data) => {
-        debugger
-      },
-      received: (data) => {
-        debugger
-      },
-      updatePlayers: (data) => {
-        debugger
-      }
+  App.room = App.cable.subscriptions.create({
+    channel, roomId,
+    received: function (data) {
+      debugger
     }
-  )
+  })
 }
