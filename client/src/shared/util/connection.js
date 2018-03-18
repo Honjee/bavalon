e5734a2d69dfe2c04d34d3d323387a21d84c42fa
royce <kim.royce2@gmail.com>
2018-03-18 19:50:29 -0400

    fix websocket create subscription

--- client/src/shared/util/connection.js
+++ client/src/shared/util/connection.js
@@ -2,11 +2,18 @@ import ActionCable from 'actioncable'
 
 export const createConnection = (channel, roomId) => {
   const App = {}
-  App.cable = ActionCable.createConsumer()
-  App.room = App.cable.subscriptions.create({
-    channel, roomId,
-    received: function (data) {
-      debugger
+  App.cable = ActionCable.createConsumer('ws://localhost:3001/cable')
+  debugger
+  App.room = App.cable.subscriptions.create(
+    {
+      channel,
+      roomId
+    },
+    {
+      received: data => {
+        console.log(data)
+        debugger
+      }
     }
-  })
+  )
 }
