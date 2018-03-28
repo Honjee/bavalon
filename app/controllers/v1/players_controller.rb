da3a06e2b5ad62150c65decbea8d07fc86b73b01
royce <kim.royce2@gmail.com>
2018-03-27 20:14:55 -0400

    refactor broadcast

--- app/controllers/v1/players_controller.rb
+++ app/controllers/v1/players_controller.rb
@@ -10,7 +10,7 @@ class V1::PlayersController < ApplicationController
     @players = Player.new()
     @players.room = Room.find(params[:room_id])
     @players.owner_id = @players.room.owner_id
-    @players.players = ''
+    @players.players = ""
     if @players.save
       render 'v1/players/show'
     else
@@ -24,16 +24,18 @@ class V1::PlayersController < ApplicationController
     previous_players = @players.players || ""
 
     if params[:status] == 'JOIN'
-      updated_players = previous_players.split(',')
-      return render json: [error: 'Room is full'], status: 422 if updated_players.count > 10
-      updated_players = previous_players.split(',').push(params[:playername]).uniq.join(',')
+      parsed_players = previous_players.split(',')
+      return render json: [error: 'Room is full'], status: 422 if previous_players.length > 10
+      updated_players = parsed_players.push(params[:playername]).uniq.join(',')
     elsif params[:status] == 'REMOVE'
-      updated_players = previous_players.split(',').dup.delete(params[:playername])
+      updated_players = previous_players.join(',').dup.delete(params[:playername])
     end
 
     @players.players = updated_players
     if @players.save
-      broadcast_players @players
+
+      #send ws broadcast of new players
+      broadcast_players_to_room(@players)
       render 'v1/players/show'
     else
       render json: @players.errors.full_messages, status: 422
@@ -41,6 +43,14 @@ class V1::PlayersController < ApplicationController
   end
 
   private
+
+  def broadcast_players_to_room(players)
+    ActionCable.server.broadcast(
+      "room_#{@players.room.id}",
+      roomId: @players.room.id,
+      players: @players.players
+    )
+  end
 
   def player_params
     params.require(:players).permit(:room_id, :players, :owner_id, :playerName)
