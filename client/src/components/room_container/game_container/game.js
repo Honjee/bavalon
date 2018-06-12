2362217c72b97177dc7afef140b7f94a2d8a9f0c
royce <kim.royce2@gmail.com>
2018-06-11 20:04:12 -0400

    add props and state to Game

--- client/src/components/room_container/game_container/game.js
+++ client/src/components/room_container/game_container/game.js
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types'
 // import Style from './style.css'
 
 class Game extends React.Component {
+  componentDidMount() {
+    this.props.fetchRoom(this.props.roomName)
+  }
+
   render() {
     return (
-      <div/>
+      <div>{'GAMEHERE'}</div>
     )
   }
 }
 
+Game.propTypes = {
+  fetchRoom: PropTypes.func,
+  roomId: PropTypes.string,
+  roomName: PropTypes.string,
+  room: PropTypes.object
+}
 export default Game
--- client/src/components/room_container/game_container/index.js
+++ client/src/components/room_container/game_container/index.js
@@ -2,17 +2,26 @@ import Game from './game'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
+import { getRoom } from '../../home_container/create_room_container/actions'
+
 const mapStateToProps = (state, ownProps) => {
   const players = state.getIn(['players', 'players'])
   const session = state.getIn(['session'])
   const [ ...keys ] = session.keys()
   const userName = session.getIn([keys[0], 'username'])
   const room = state.getIn(['room', 'room'])
+
+  return {
+    players,
+    room,
+    roomName,
+    userName
+  }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    fetchRoom: room => dispatch(getRoom(room))
   }
 }
 
